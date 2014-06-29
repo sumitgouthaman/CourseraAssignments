5a2844a2f84095ed7eeed109c9c6b23fcf5a1d1f
Sumit Gouthaman <sumitgt007@gmail.com>
2014-06-29 16:22:00 +0530

    (Algorithms1, week3) Solution scaffolding

---
+++ Algorithms 1/Week 3/Pattern Recognition/Brute.java
@@ -0,0 +1,15 @@
+/**-----------------------------------------------------------------------------
+ * Author:           Sumit Gouthaman
+ * Written:          30 June 2014
+ * Last updated:     30 June 2014
+ * 
+ * Compilation:      javac-algs4 Brute.java
+ * Execution:        java-algs4 Brute
+ * 
+ * Uses brute force to calculate collinear points in a plane
+ * 
+ *--------------------------------------------------------------------------- */
+
+public class Brute {
+   public static void main(String[] args)
+}
---
+++ Algorithms 1/Week 3/Pattern Recognition/Fast.java
@@ -0,0 +1,15 @@
+/**-----------------------------------------------------------------------------
+ * Author:           Sumit Gouthaman
+ * Written:          30 June 2014
+ * Last updated:     30 June 2014
+ * 
+ * Compilation:      javac-algs4 Fast.java
+ * Execution:        java-algs4 Fast
+ * 
+ * Fast algorithms to find collinear points in a plane.
+ * 
+ *--------------------------------------------------------------------------- */
+
+public class Fast {
+   public static void main(String[] args)
+}
